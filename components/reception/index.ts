82283e546604f2251f73106996f592e9f4016786
MikelNoppo <mikelnoppo03@gmail.com>
2025-07-29 00:51:11 +0800

    feat: remove unused reception components and update exports

--- components/reception/index.ts
+++ components/reception/index.ts
@@ -1,5 +1,2 @@
-export { ReceptionStatsCards } from "./reception-stats"
-export { ReceptionTable } from "./reception-table"
 export { AddReceptionDialog } from "./add-reception-dialog"
-export { SearchBar } from "./search-bar"
 export { CreateReceptionDialog } from './create-reception-dialog'
